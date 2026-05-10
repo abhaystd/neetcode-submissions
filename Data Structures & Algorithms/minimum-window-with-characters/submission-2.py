951ec4f0399c194bbb92ef69481233e71a8eeb64
Abhay Yadav <159945778+abhaystd@users.noreply.github.com>
2026-05-10 23:00:00 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t=='':
+            return ''
+
+        freq=defaultdict(int)
+        for c in t:
+            freq[c] +=1 
+
+        matched=0
+        req=len(freq)
+
+        res, reslen = [-1, -1], float('inf')
+        freqs=defaultdict(int)
+        l=0
+        for i in range(len(s)):
+            if freq[s[i]]!=0:
+                freqs[s[i]]+=1
+                if freqs[s[i]]==freq[s[i]]:
+                    matched+=1
+
+            while matched == req and l<=i:
+                if reslen > i-l+1:
+                    reslen = i-l+1  
+                    res=[l,i]
+                if freq[s[l]]!=0:
+                    freqs[s[l]] -= 1
+                    if freqs[s[l]] <freq[s[l]]:
+                        matched-=1
+                l+=1      
+
+        l, r =res
+        # TC O(n) and SC T(n)
+        return s[l:r+1] if reslen != float('inf') else ''
+
+
+            
+            
+            
+
