f52ed4539a064133794f3820cc31cc31867aebd3
Abhay Yadav <159945778+abhaystd@users.noreply.github.com>
2026-05-13 22:20:39 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.arr=[]
+        self.min_arr=[]
+
+    def push(self, val: int) -> None:
+        stack=self.arr
+        stack.append(val)
+
+        if len(self.min_arr)!=0 and self.min_arr[-1]>=val:
+            self.min_arr.append(val)
+        elif len(self.min_arr)==0:
+            self.min_arr.append(val)
+
+    def pop(self) -> None:
+        top_val=self.arr[-1]
+        self.arr.pop()
+        if top_val == self.min_arr[-1]:
+            self.min_arr.pop()
+
+        
+    def top(self) -> int:
+        return self.arr[-1]
+
+    def getMin(self) -> int:
+        return self.min_arr[-1]
+        
+    # all operation is done in TC O(n) and SC(n)
