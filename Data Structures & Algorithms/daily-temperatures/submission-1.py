c10cf83c694626880f9664d0ac91a24d338ab717
Abhay Yadav <159945778+abhaystd@users.noreply.github.com>
2026-05-15 22:46:37 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res=[]
+        for i in range(0,len(temperatures)):
+            count=0
+            j=i+1
+            while j<len(temperatures):
+                if temperatures[i]<temperatures[j]:
+                    break;
+                count+=1
+                j+=1
+            count = 0 if j==len(temperatures) else count+1
+            res.append(count)
+        #  BRUTE FORCE TC O(n^2) SC O(1)
+        return res
