b5e6f8411c7cceb8c79c6e4c7479b173d0f37c96
Abhay Yadav <159945778+abhaystd@users.noreply.github.com>
2026-05-19 21:58:12 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def is_possible(self, mid:int, piles:List[int], h:int) ->bool:
+        total_hours=0
+        for pile in piles:
+            total_hours+=(pile+mid-1)//mid
+        return total_hours<=h
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        max_eat=max(piles)
+        min_eat=1
+        
+        while min_eat<=max_eat:
+            mid=(min_eat+max_eat)//2
+            if self.is_possible(mid,piles,h):
+                print(mid)
+                max_eat = mid-1
+            else:
+                min_eat = mid+1
+
+        return min_eat
+
