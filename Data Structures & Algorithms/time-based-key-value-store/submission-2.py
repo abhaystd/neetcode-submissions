76e973abfeb5ae178f8f20150933b67d57f97431
Abhay Yadav <159945778+abhaystd@users.noreply.github.com>
2026-05-22 22:21:36 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,29 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keyTimeMap= defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        timeMap=self.keyTimeMap
+        timeMap[key].append((timestamp,value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        timeMap=self.keyTimeMap
+        l=0
+        values=timeMap.get(key,[])
+        if len(values) ==0:
+            return ''
+        r=len(values)-1
+        res=''
+        while l <= r:
+            mid = (l+r)//2
+            if timestamp>=values[mid][0]:
+                l = mid+1
+                res = values[mid][1]
+            else:
+                r = mid-1
+        #  for set TC O(1) for get TC O(log n)
+        # SC is O(n*m) where n is key and m is values
+        return res
+
+
