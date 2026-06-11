f082e035df456183338afdf7adced7724a8dd363
Abhay Yadav <159945778+abhaystd@users.noreply.github.com>
2026-06-11 22:13:56 +0530

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        q=deque()
+        q.append(root)
+        res=[]
+        while q:
+            l=len(q)
+            res.append(q[-1].val)
+            for i in range(l):
+                curr=q.popleft()
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+        # TC O(N) AND SC O(N)
+        return res
+
