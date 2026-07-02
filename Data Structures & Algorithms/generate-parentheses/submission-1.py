000509443702d170ab062c54f102b398a2e042a9
Abhay Yadav <159945778+abhaystd@users.noreply.github.com>
2026-07-02 21:38:37 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        self.res=[]
+    def generateALL(self,comb: List[str],o:int,c:int,n:int):
+        if o==n and c==n:
+            self.res.append("".join(comb))
+            return
+        
+        if o<n:
+            comb.append('(')
+            self.generateALL(comb,o+1,c,n)
+            comb.pop()
+        if c<o:
+            comb.append(')')
+            self.generateALL(comb,o,c+1,n)
+            comb.pop()
+
+        return
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.generateALL([],0,0,n)
+        # TC O(4^N/M^(1/2)) AND SC O(N)
+        return self.res
