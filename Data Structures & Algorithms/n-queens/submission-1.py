4935adab975a71694db531755a36b6034e77ca9c
Abhay Yadav <159945778+abhaystd@users.noreply.github.com>
2026-07-09 00:49:19 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def __init__(self):
+        self.res=[]
+    def is_safe(self,n,board,row,i):
+        r=row-1
+        while r>=0:
+            if board[r][i]=='Q':
+                return False
+            r-=1
+
+        r=row-1
+        c=i-1
+        while r>=0 and c>=0:
+            if board[r][c]=='Q':
+                return False
+            r-=1
+            c-=1
+        r=row-1
+        c=i+1
+        while r>=0 and c<n:
+            if board[r][c]=='Q':
+                return False
+            r-=1
+            c+=1
+        return True
+    def queenBoard(self,n,board,row):
+        if row==n:
+            copy=[''.join(row) for row in board]
+            self.res.append(copy)
+            return
+        for col in range(n):
+            if self.is_safe(n,board,row,col):
+                board[row][col]='Q'
+                self.queenBoard(n,board,row+1)
+                board[row][col]='.'
+        return
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board=[['.']*n for i in range(n)]
+        self.queenBoard(n,board,0)
+        # tc O(n!) and sc O(n^2)
+        return self.res
