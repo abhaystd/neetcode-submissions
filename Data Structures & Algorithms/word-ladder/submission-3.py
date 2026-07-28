447dfe31ac65578f1ba1e924d60ceaa960554dd7
Abhay Yadav <159945778+abhaystd@users.noreply.github.com>
2026-07-28 23:09:28 +0530

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.py
@@ -0,0 +1,53 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if beginWord == endWord or endWord not in wordList:
+            return 0
+        adj=defaultdict(list)
+        n=len(beginWord)
+        m=len(wordList)
+        for i in range(m):
+            count=0
+            for j in range(n):
+                if wordList[i][j]!=beginWord[j]:
+                    count+=1
+                if count>1:
+                    break
+            if count==1:
+                adj[beginWord].append(i)
+
+        for i in range(m):
+            for j in range(i+1,m):
+                count=0
+                for k in range(n):
+                    if wordList[i][k]!=wordList[j][k]:
+                        count+=1
+                    if count>1:
+                        break
+                if count==1:
+                    adj[wordList[i]].append(j)
+                    adj[wordList[j]].append(i)
+               
+        res=1
+        queue=deque()
+        vis=set()
+        for neg in adj[beginWord]:
+            if neg not in vis:
+                vis.add(neg)
+                queue.append(neg)
+
+
+        while queue:
+            res+=1
+            l=len(queue)
+            for i in range(l):
+                node = queue.popleft()
+                
+                if wordList[node] == endWord:
+                    return res
+
+                for neg in adj[wordList[node]]:
+                    if neg not in vis:
+                        queue.append(neg)
+                        vis.add(neg)
+        # TC O(m^2*n) where n is len of words and m is no of words and SC O(M^2)
+        return 0
